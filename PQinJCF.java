39b83466d97729a7a820011ef686134af9959f17
Akanksha <228480495+arpitgit2006@users.noreply.github.com>
2026-07-23 22:42:10 +0530

    priority queue in java collection framework

--- PQinJCF.java
+++ PQinJCF.java
@@ -1,3 +1,26 @@
+import java.util.*;
 public class PQinJCF {
-    
+    public static void main(String[] args) {
+      PriorityQueue<Integer> pq=new PriorityQueue<>();
+      PriorityQueue<Integer> pqrev=new PriorityQueue<>(Comparator.reverseOrder());
+      pq.add(4);
+      pq.add(2);
+      pq.add(7);
+      pq.add(3);
+      pq.add(5);
+      while(!pq.isEmpty()){
+        System.out.print(pq.peek()+" ");
+        pq.remove();
+      }
+      System.out.println();
+      pqrev.add(4);
+      pqrev.add(2);
+      pqrev.add(7);
+      pqrev.add(3);
+      pqrev.add(5);
+      while(!pqrev.isEmpty()){
+        System.out.print(pqrev.peek()+" ");
+        pqrev.remove();
+      }
+    }
 }
